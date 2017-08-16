6a6952aad45c64aecf95d5f04d4d54a89dcd8edf
Tahim Kader <tahim.kader@gmail.com>
2017-08-15 23:19:04 -0700

    Added Stripe Payment component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+//With the StripeCheckout there are requirements needed:
+//name is the header of the checkout box
+//amount (defaults USD) and in units of cents
+//token is expecting the authorization token back (callback). The callback function will run when we receive the token from Stripe
+//StripeKey is the publishable api key
+class Payments extends Component {
+  render(){
+    return (
+      <StripeCheckout
+        name = "Emaily"
+        description = "Pay 5 bucks for survey"
+        amount ={500}
+        token = {token => console.log(token)}
+        stripeKey = {process.env.REACT_APP_STRIPE_KEY}
+      >
+      <button className= "btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
